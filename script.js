5f6159f1733473f1c7d36581036058c586829e6b
Badie Alili <yacineabdelbadiealili@gmail.com>
2024-02-06 21:50:26 +0100

    Add eraser and selected tool logic

--- script.js
+++ script.js
@@ -1,60 +1,66 @@
 // Global
 let gridSize = 32;
-let color = "#000000"
-
+let color = "#000000";
+let selected = "Pen";
 // Add grid squares to page
 function initializeGrid() {
-    grid.innerHTML = "";
-    let frag = new DocumentFragment();
-    for (i = 0; i < gridSize ** 2; i++) {
-        let div = document.createElement('div');
-        div.classList.add('grid-square');
-        div.style.width = 480 / gridSize + 'px';
-        div.style.height = 480 / gridSize + 'px';
-        frag.append(div);
-    }
-    grid.append(frag);
+  grid.innerHTML = "";
+  let frag = new DocumentFragment();
+  for (i = 0; i < gridSize ** 2; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid-square");
+    div.style.width = 480 / gridSize + "px";
+    div.style.height = 480 / gridSize + "px";
+    frag.append(div);
+  }
+  grid.append(frag);
 }
 initializeGrid();
 
 // Color grid square
-grid.addEventListener('mouseover', (event) => {
-    event.preventDefault();
-    if (!event.target.classList.contains('grid-square'))
-        return;
+grid.addEventListener("mouseover", (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("grid-square")) return;
 
-    if (event.buttons !== 1)
-        return;
+  if (event.buttons !== 1) return;
 
-    console.log(event.buttons)
-    event.target.style.backgroundColor = color;
-})
+  if (selected == "Pen") event.target.style.backgroundColor = color;
+  else event.target.style.backgroundColor = "transparent";
+});
 
 // Prevent mouse dragging colored divs
-grid.addEventListener('mousedown', (event) => {
-    if (!event.target.classList.contains('grid-square'))
-        return;
-    event.preventDefault();
-})
+grid.addEventListener("mousedown", (event) => {
+  if (!event.target.classList.contains("grid-square")) return;
+  event.preventDefault();
+});
 
 // Change color
-colorPicker = document.getElementById('color-picker');
-colorPicker.addEventListener('change', (event) => {
-    color = event.target.value;
-})
+colorPicker = document.getElementById("color-picker");
+colorPicker.addEventListener("change", (event) => {
+  color = event.target.value;
+});
 
 // Clear grid
 function clearGrid() {
-    let gridSquareList = grid.children;
-    for (gridSquare of gridSquareList) {
-        gridSquare.style.backgroundColor = 'transparent';
-    }
+  let gridSquareList = grid.children;
+  for (gridSquare of gridSquareList) {
+    gridSquare.style.backgroundColor = "transparent";
+  }
 }
 
-clear.addEventListener('click', clearGrid);
+clear.addEventListener("click", clearGrid);
 
 // Resize grid
-size.addEventListener('change', (event)=>{
-    gridSize = event.target.value;
-    initializeGrid();
+size.addEventListener("change", (event) => {
+  gridSize = event.target.value;
+  initializeGrid();
+});
+
+// Toggle selection between pen and eraser
+eraser.addEventListener('click', ()=>{
+    selected = "Eraser";
+});
+
+pen.addEventListener('click', ()=>{
+    selected = "Pen";
 })
